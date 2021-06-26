c5a4a6d1d067886970d0bea3fbecc3f4f36393ec
Bai_Ding <50022035+HuangC7@users.noreply.github.com>
2021-06-26 21:06:28 +0800

    js

---
+++ js/admin.js
@@ -0,0 +1,16 @@
+
+var userData = [
+    {
+        userName :'admin_0023',
+        pwd:'123456789'
+    },
+    {
+        userName :'admin_001',
+        pwd:'123456789'
+    },
+    {
+        userName :'admin_002',
+        pwd:'123456789'
+    }
+
+]
---
+++ js/data.js
@@ -0,0 +1,76 @@
+
+var pData = [
+    {
+        img:"./images/01.jpg",
+        bimg:"./images/01big.jpg",
+        tiitle: '袋鼠妈妈 星钻光感柔雾口红 孕妇专用口红孕期化妆品植物孕妇化妆品孕妇彩妆',
+        price: 132,
+        mcolor: ['红色','黑色'],
+        mchifa: ['X','XX','XXL','MML'],
+        mcount: 1
+    },
+    {
+        img:"./images/02.jpg",
+        bimg:"./images/02big.jpg",
+        tiitle: '泊泉雅化妆品彩妆套装正品全套美妆工具女学生送礼礼盒',
+        price: 78,
+        mcolor: ['红色','黑色'],
+        mchifa: ['X','XX','XXL','MML'],
+        mcount: 1
+    },
+    {
+        img:"./images/03.jpg",
+        bimg:"./images/03big.jpg",
+        tiitle: '【专柜正品】纪梵希Givenchy 轻盈无痕四四色散粉 1号色慕斯淡彩 定妆粉 控',
+        price: 470,
+        mcolor: ['红色','黑色'],
+        mchifa: ['X','XX','XXL','MML'],
+        mcount: 1
+    },
+    {
+        img:"./images/04.jpg",
+        bimg:"./images/04big.jpg",
+        tiitle: '卡姿兰（Carslan）精准双头美颜笔（遮瑕液2.6g+遮瑕膏4.5g）',
+        price: 55,
+        mcolor: ['红色','黑色'],
+        mchifa: ['X','XX','XXL','MML'],
+        mcount: 1
+    },
+    {
+        img:"./images/01.jpg",
+        bimg:"./images/01big.jpg",
+        tiitle: '袋鼠妈妈 星钻光感柔雾口红 孕妇专用口红孕期植物孕妇化妆品孕妇彩妆',
+        price: 132,
+        mcolor: ['红色','黑色'],
+        mchifa: ['X','XX','XXL','MML'],
+        mcount: 1
+    },
+    {
+        img:"./images/02.jpg",
+        bimg:"./images/02big.jpg",
+        tiitle: '泊泉雅化妆品彩品全套老国货品牌抖音初学者美妆工具女学生送礼礼盒',
+        price: 78,
+        mcolor: ['红色','黑色'],
+        mchifa: ['X','XX','XXL','MML'],
+        mcount: 1
+    },
+    {
+        img:"./images/03.jpg",
+        bimg:"./images/03big.jpg",
+        tiitle: '【专柜正品】纪梵希Givenchy 轻盈格四色散粉 1号色慕斯淡彩 定妆粉 控',
+        price: 470,
+        mcolor: ['红色','黑色'],
+        mchifa: ['X','XX','XXL','MML'],
+        mcount: 1
+    },
+    {
+        img:"./images/04.jpg",
+        bimg:"./images/04big.jpg",
+        tiitle: '卡姿兰（Carslan）精准美颜笔（遮瑕液2.6g+遮瑕膏4.5g）',
+        price: 55,
+        mcolor: ['红色','黑色'],
+        mchifa: ['X','XX','XXL','MML'],
+        mcount: 1
+    },
+
+]
